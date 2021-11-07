56f515506a62fb99d094cda04c1aec0ae5b8d10c
dolnya <90932597+dolnya@users.noreply.github.com>
2021-11-07 10:14:08 +0100

    Instrukcja do zadania :( wykasowałe to co wczesniej miąłem

--- Program.cs
+++ Program.cs
@@ -5,7 +5,17 @@ namespace sda_csharp_exercises
     class Program
     {
         static void Main(string[] args)
+
+            //E01
+
+            //Napisz metodę, która będzie wyświetlała prosty cennik danego produktu.
+            //W zależności od ceny produktu oraz liczby linii, którą ktoś sobie życzy widzieć wydrukuj cennik w
+            //postaci:
+            //Liczba sztuk: 1 łączna cena: 5zł
+            //Liczba sztuk: 2 łączna cena: 10zł
+            //Liczba sztuk: 3 łączna cena: 15zł
         {
+
         }
     }
 }

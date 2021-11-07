262b05608d81a72f6208ac6c6f63db45af725351
dolnya <90932597+dolnya@users.noreply.github.com>
2021-11-07 10:28:24 +0100

    Zadanko 3

--- Program.cs
+++ Program.cs
@@ -5,7 +5,22 @@ namespace sda_csharp_exercises
     class Program
     {
         static void Main(string[] args)
+        // E03
+        //Napisz funkcję tworzącą ciąg arytmetyczny o podanych: długości, pierwszym elemencie, różnicy ciągu.
         {
+            Console.WriteLine("Wpis długość");
+            int lenght = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Wpisz pierwszy element");
+            int first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Wpisz różnicę ciągu");
+            int element = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ciąg =>");
+            Console.Write($" {first}");
+            for (int i =1; i<lenght; i++)
+            {
+                Console.Write($" {first+(i*element)}");
+            }
+                
         }
     }
 }

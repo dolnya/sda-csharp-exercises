6610cf5541b443e8554cb657ab8a442b7397bf8f
dolnya <90932597+dolnya@users.noreply.github.com>
2021-11-07 15:40:56 +0100

    Zadanko 11

--- Program.cs
+++ Program.cs
@@ -5,7 +5,31 @@ namespace sda_csharp_exercises
     class Program
     {
         static void Main(string[] args)
+        //E11
+        //Napisz funkcję, która w zależności od przekazanych parametrów wydrukuje spersonalizowany tekst
+        //jak poniżej.
+        //W nawiasach kwadratowych oznaczyłem elementy, które mają być spersonalizowane:
+        //[Imię Drugie_Imię Nazwisko]
+        //[ul.Nazwa_ulicy Numer / numer_mieszkaniaMiasto]
+        //[Szanowna Pani | Szanowny Panie | Droga Użytkowniczko | Drogi Użytkowniku]
+        //Zapraszamy[Pana | Panią | Cię] serdecznie na otwarcie naszego sklepu!
+        //Powitamy[Pana | Panią | Cię][kieliszkiem szampana | kubkiem gorącej czekolady].
+        //[I zapraszamy do gry w bingo]
+        //[do zobaczenia | czekamy na Pana | czekamy na Panią | z wyrazami szacunku]
+        //Założenia:
+        //- w zależności od płci i wieku wyświetlamy odpowiednie powitanie i później formy(Pan lub Pani, a
+        //dla niepełnoletnich Ty i Użytkowniku lub Użytkowniczko
+        //- dla niepełnoletnich czekolada, dla pełnoletnich szampan
+        //- gra w bingo tylko dla seniorów (powyżej 60 lat)
+        //- zakończenie – „z wyrazami szacunku” dla seniorów, pozostałe w zalezności od płci i wieku
+        //- płeć powinna być przekazana jako parametr funkcji, możesz np.użyć odpowiednio liter ‘m’ i ‘k’
+        //lub liczb i założyć, że np. 1 to kobieta, 2 to mężczyzna, lub jako zmienna logiczna, np.isFemale
+        //- pamiętaj, że nie każdy ma drugie imię i numer mieszkania.
         {
+
+
+
+
         }
     }
 }

ab2a8f18bbd80b59eaa0b6118951596756247a91
dolnya <90932597+dolnya@users.noreply.github.com>
2021-11-17 20:12:42 +0100

    Opis

--- Program.cs
+++ Program.cs
@@ -5,7 +5,18 @@ namespace sda_csharp_exercises
     class Program
     {
         static void Main(string[] args)
-        {
+        //    E13
+        //    Napisz program liczący harmonogram spłaty rat kredytu hipotecznego wg algorytmu podanego na
+        //    stronie https://hipoteki.net/wiedza/jak-obliczyc-raty-i-odsetki-od-kredytu-hipotecznego/
+        //    Weź pod uwagę obliczenia dla raty MALEJĄCEJ(drugi algorytm).
+        //    Dla podanych: kwoty kredytu, liczby miesięcy oraz oprocentowania wypisz wysokość
+        //    poszczególnych rat oraz całkowitą kwotę do spłaty(czyli sumę wszystkich rat).
+        //    Nie martw się zaokrągleniami!
+        //    UWAGA!!! W podanym algorytmie jest drobna nieścisłość: tam, gdzie jest napisane 4,7 oznacza to
+        //    tak naprawdę 4,7%
+        //    Obliczenia możesz sprawdzić porównując swoje wyniki z tymi ze strony:
+        //    https://www.bankier.pl/narzedzia/kalkulator-kredytowy
+        //{
         }
     }
 }

410854a49cf6f6ddee932a10bdfae34e74dcc17f
dolnya <90932597+dolnya@users.noreply.github.com>
2021-11-17 20:11:50 +0100

    Opis

--- Program.cs
+++ Program.cs
@@ -5,6 +5,16 @@ namespace sda_csharp_exercises
     class Program
     {
         static void Main(string[] args)
+            //Napisz funkcję, która dla podanego zdania wypisze statystykę dotyczacą długości występujących w
+            //nich słów.
+            //Każda linia powinna zawierać informację o długości słów, następnie wymienione wszystkie słowa
+            //o takiej długości oraz liczbę tych słów.
+            //Przykład:
+            //dla „ala i ola mają koty”
+            //wyświetlamy:długość 1 – i – liczba słów: 1
+            //długość 2 - - liczba słów: 0
+            //długość 3 – ala ola – liczba słów: 2
+            //długość 4 – mają koty – liczba słów: 2.
         {
         }
     }

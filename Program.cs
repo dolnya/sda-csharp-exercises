83d0ad699371148060c4e1f0138dbac7044c9f1b
dolnya <90932597+dolnya@users.noreply.github.com>
2021-11-20 09:21:27 +0100

    Comment

--- Program.cs
+++ Program.cs
@@ -5,6 +5,9 @@ namespace sda_csharp_exercises
     class Program
     {
         static void Main(string[] args)
+        //E21
+        //Rozwiąż zadanie E20 bez wykorzystywania dziedziczenia(utwórz jedną klasę BasicAccount).
+        //Wskaż zalety i wady każdego z podejść.
         {
         }
     }

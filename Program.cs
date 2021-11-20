c62829a1cd9082ec66006b3b2c2e2739c2f4fc29
dolnya <90932597+dolnya@users.noreply.github.com>
2021-11-20 13:23:21 +0100

    W oparciu o 14

--- Program.cs
+++ Program.cs
@@ -5,55 +5,12 @@ namespace sda_csharp_exercises
 {
     class Program
     {
-        static void PrintHello(Person person)
-        {
-            if (person.IsAdult())
-            {
-                Console.WriteLine("Welcome to liquor store");
-            }
-            else
-            {
-                Console.WriteLine("Welcome to toy store");
-            }
-        }
+        //Programowanie obiektowe - polimorfizm
+        //E16(realizuje trener)
+        //W twojej firmie manager ma 10% dodatek do swojego wynagrodzenia.
+        //Utwórz listę pracowników oraz managerów i wyświetl wynagrodzenie dla każdego z nich.
         static void Main(string[] args)
-        //E14(realizuje trener)
-        //Jesteś pracodawcą, który chce zorganizować imprezę firmową.
-        //Potrzebujesz stworzyć listę uczestników – niektórzy z nich będą twoimi pracownikami, niektórzy
-        //nie.
-        //Wyświetl informację o wszystkich uczestnikach imprezy, jeżeli któryś uczestnik jest równocześnie
-        //pracownikiem – wyświetl jego wynagrodzenie.
-
-        {
-            Person person1 = new Person("Jan","Kowalski",1999);
-            Employee employee1 = new Employee("Adam","Nowak",2000,5000);
-            Person person2 = new Employee("Joanna", "Kowalska", 2000, 5000);
-            Person person3 = new Person("Adam", "Wielki", 2010);
-            List<Person> people = new List<Person>();
-            people.Add(person1);
-            people.Add(employee1);
-            people.Add(person2);
-            people.Add(person3);
-            foreach (Person p in people)
-            {
-                if (p is Employee)
-                {
-                    p.WhoAmI();
-                    Console.WriteLine("Is Employee");
-                }
-                else if (p is Person)
-                {
-                    p.WhoAmI();
-                    Console.WriteLine("Is Person");
-
-                }
-                else
-                {
-                    Console.WriteLine("No idea ?");
-                }
-                PrintHello(p);
-
-            }
+        { 
         }
     }
 

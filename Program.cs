cd3a64f4bbd9bcb8e85bd01b670305a8853bc0fe
dolnya <90932597+dolnya@users.noreply.github.com>
2021-11-20 09:22:34 +0100

    Comment

--- Program.cs
+++ Program.cs
@@ -5,6 +5,13 @@ namespace sda_csharp_exercises
     class Program
     {
         static void Main(string[] args)
+        //E22
+        //Jesteś malarzem.
+        //Twój klient chce żebyś mu namalował na ścianach koła(Circle) i kwadraty(Square).
+        //Przekazał Ci listę wymiarów wszystkich kształtów do namalowania(Shape). W przypadku kół
+        //znasz ich promień(radius), w przypadku kwadratów długość boku(side).
+        //Musisz ustalić ile farby potrzebujesz, w tym celu musisz najpierw ustalić łączną powierzchnie
+        //wszystkich kształtów.
         {
         }
     }

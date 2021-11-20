cf23adc7e1bbbc7ef0deb96c192b0aaf71d4c536
dolnya <90932597+dolnya@users.noreply.github.com>
2021-11-20 09:10:11 +0100

    Comment

--- Program.cs
+++ Program.cs
@@ -5,6 +5,12 @@ namespace sda_csharp_exercises
     class Program
     {
         static void Main(string[] args)
+        //E14(realizuje trener)
+        //Jesteś pracodawcą, który chce zorganizować imprezę firmową.
+        //Potrzebujesz stworzyć listę uczestników – niektórzy z nich będą twoimi pracownikami, niektórzy
+        //nie.
+        //Wyświetl informację o wszystkich uczestnikach imprezy, jeżeli któryś uczestnik jest równocześnie
+        //pracownikiem – wyświetl jego wynagrodzenie.
         {
         }
     }

03d189ca82b44ba9c79759ae8284069dfaac8327
dolnya <90932597+dolnya@users.noreply.github.com>
2021-11-20 09:20:29 +0100

    Comment

--- Program.cs
+++ Program.cs
@@ -5,6 +5,26 @@ namespace sda_csharp_exercises
     class Program
     {
         static void Main(string[] args)
+        //E20
+        //Jesteś managerem w banku.
+        //Twoim celem jest ustalenie ile pieniędzy jest przechowywanych na wszystkich kontach w banku.
+        //Istnieją dwa rodzaje kont: konto bieżące(Account) oraz konto oszczędnościowe(SavingAccount).
+        //Oba rodzaje kont posiadają informacje na temat swojego salda(balance), ale w przypadku konta
+        //oszczędnościowego mamy dodatkowo informację o odsetkach(interest), które są aktualizowane
+        //codziennie – czyli w przypadku konta oszczędnościowego by ustalić jego rzeczywisty stan trzeba
+        //dodać informację o odsetkach.
+        //Saldo i odsetki powinny być publicznymi właściwościami.
+        //Ukryj szczegóły implementacji oraz zablokuj możliwość bezpośredniego ustawiania salda konta.
+        //Stwórz odpowiednie konstruktory oraz dodaj metodę IncreaseInterest do konta oszczędnościowego
+        //(ta metoda powinna mieć prostą implementację, która będzie zwiększała wartość odsetek o wartość
+        //przekazanego parametru – szczegóły tego jak liczymy odsetki nie są w tej chwili istotne).
+        //Dodaj do obu klas metodę IncreaseBalance, która zwiększy stan salda o wartość przekazaną jako
+        //parametr.
+        //Przetestuj swoje rozwiązanie w działaniu.
+        //Utwórz klasę reprezentującą bank (Bank), która będzie przechowywała listę wszystkich kont, a w
+        //niej metodę AddAccount, która będzie dodawała do listy informację o nowym koncie.W tej samej
+        //klasie stwórz metodę GetValue(), która będzie dostarczała informacji o tym ile pieniędzy jest
+        //przechowywane na wszystkich kontach.
         {
         }
     }

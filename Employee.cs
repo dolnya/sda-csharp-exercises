61c5a63fc2c9b7ed94d94c0c6b92620581ceaece
dolnya <90932597+dolnya@users.noreply.github.com>
2021-11-20 11:11:33 +0100

    Zadanko

---
+++ Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sda_csharp_exercises
+{
+    class Employee : Person
+    {
+        public decimal Salary
+        {
+            get; set;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,7 +11,32 @@ namespace sda_csharp_exercises
         //nie.
         //Wyświetl informację o wszystkich uczestnikach imprezy, jeżeli któryś uczestnik jest równocześnie
         //pracownikiem – wyświetl jego wynagrodzenie.
+        //{
+        //    Person person = new Person();
+        //    person.WhoAmI();
+        //    person.FirstName = "Jan";
+        //    person.LastName = "Kowalski";
+        //    person.YearOfBirth = 1981;
+        //    Console.WriteLine(person.IsAdult());
+        //}
         {
+            Person person = new Person()
+            {
+                FirstName = "Jan",
+                LastName = "Kowalski",
+                YearOfBirth = 1981,
+
+            };
+
+
+            Employee employee = new Employee();
+            {
+                employee.FirstName = "Andrzej";
+                employee.LastName = "Kowalak";
+                employee.YearOfBirth = 1941;
+                employee.Salary = 2000;
+            };
         }
+
     }
 }

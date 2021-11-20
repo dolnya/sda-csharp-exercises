4fbb7b3c5da4a6e2d5c2724af0fa5c4fd364f302
dolnya <90932597+dolnya@users.noreply.github.com>
2021-11-20 09:18:59 +0100

    Comment

--- Program.cs
+++ Program.cs
@@ -5,6 +5,9 @@ namespace sda_csharp_exercises
     class Program
     {
         static void Main(string[] args)
+        //Programowanie obiektowe – enkapsulacja
+        //E18(realizuje trener)
+        //Wykorzystaj odpowiednio enkapsulację modyfikując rozwiązania zadań E14 i E16.
         {
         }
     }

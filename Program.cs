b47c1ec402259dd30427996cbfb0eeb2bf171e8a
dolnya <90932597+dolnya@users.noreply.github.com>
2021-11-20 09:12:58 +0100

    Comment

--- Program.cs
+++ Program.cs
@@ -5,6 +5,17 @@ namespace sda_csharp_exercises
     class Program
     {
         static void Main(string[] args)
+        //E15
+        //Utwórz dwie klasy do przechowywania informacji o zwierzakach: Cat i Dog.
+        //Każdy zwierzak ma imię(utwórz odpowiedni konstruktor) oraz wydaje odpowiedni dźwięk(utwórz
+        //metodę - MakeSound, która wyświetli odpowiedni tekst podobny do następującego: „Mruczek
+        //wydaje dźwięk miau”).
+        //W klasie Program utwórz kilka obiektów z każdego rodzaju zwierzaków i dodaj je do listy.
+        //Dla każdego obiektu z listy:
+        //- sprawdź jego typ(wykorzystując is lub as) i wyświetl informację o tym jaki to zwierzak(kot,
+        //pies),
+        //- wywołaj metodę MakeSound.
+        //Co musisz(musiałeś) zrobić by dodać te zwierzaki do jednej listy?
         {
         }
     }

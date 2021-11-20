c74693e58a9bed54f0b6d0aa14f74386d589a241
dolnya <90932597+dolnya@users.noreply.github.com>
2021-11-20 09:19:42 +0100

    Comment

--- Program.cs
+++ Program.cs
@@ -5,9 +5,12 @@ namespace sda_csharp_exercises
     class Program
     {
         static void Main(string[] args)
-        //Programowanie obiektowe – enkapsulacja
-        //E18(realizuje trener)
-        //Wykorzystaj odpowiednio enkapsulację modyfikując rozwiązania zadań E14 i E16.
+        //E19
+        //Pomyśl jak wykorzystując enkapsulację zmodyfikować klasy OrderItem i OrderItemWithPackage.
+        //Dlaczego nie jest dobrym pomysłem by wszystkie składowe klasy były publiczne? Podaj jakiś
+        //przykład.
+        //* Czy w przypadku właściwości prywatnych ma sens stosowanie autoimplementowanych
+        //właściwości, czy też wystarczy wykorzystać pola prywatne?
         {
         }
     }

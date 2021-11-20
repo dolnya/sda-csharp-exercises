cd0c3a060459b7e70d2cb2988f7586f00b2a2504
dolnya <90932597+dolnya@users.noreply.github.com>
2021-11-20 09:23:20 +0100

    Comment

--- Program.cs
+++ Program.cs
@@ -5,6 +5,16 @@ namespace sda_csharp_exercises
     class Program
     {
         static void Main(string[] args)
+        //Programowanie obiektowe – inne przypadki
+        //E23
+        //Utwórz klasę reprezentującą punkt(MyPoint), która będzie miała dwie właściwości: x i y.
+        //Utwórz klasę reprezentującą linię na płaszczyźnie(LineSegment), która będzie zawierała
+        //informację o dwóch obiektach punktach(reprezentujących początek i koniec linii) i będzie
+        //umożliwiała wyliczenie jej długości(Length).
+        //Utwórz dwa konstruktory:
+        //- jeden pobierający dwa punkty,
+        //- drugi cztery wartości liczbowe reprezentujące współrzędne tych punktów.
+        //Do wyliczania długości linii warto skorzystać z poniższej informacji:
         {
         }
     }

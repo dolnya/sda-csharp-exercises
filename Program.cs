46242e0813f4caaa8a13f033aa1e13f34b0c35a2
dolnya <90932597+dolnya@users.noreply.github.com>
2021-11-21 11:10:55 +0100

    Done

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection.Metadata.Ecma335;
 
 namespace sda_csharp_exercises
 {
@@ -13,6 +14,30 @@ namespace sda_csharp_exercises
         //Musisz ustalić ile farby potrzebujesz, w tym celu musisz najpierw ustalić łączną powierzchnie
         //wszystkich kształtów.
         {
+
+
+            double radius = 4.1212;
+            int numberOfCircles = 4;
+            double side = 4;
+            int numberOfSquares = 4;
+
+
+            double area = 0;
+            Console.WriteLine($"Powieszchnia kół = {(CircleArea(radius) * numberOfCircles):F1}");
+            Console.WriteLine($"Powieszchnia kwadratu = {(ReqArea(side) * numberOfSquares):F1}");
+            area = (CircleArea(radius) * numberOfCircles) + (ReqArea(radius) * numberOfSquares);
+            Console.WriteLine($"Powieszchnia wszystkiego = {(area):N}");
+
+            ;
+        }
+
+        static double CircleArea(double radius)
+        {
+           return  radius * radius * 3.14159265359;
+        }
+        static double ReqArea(double side)
+        {
+            return side * side ;
         }
     }
 }

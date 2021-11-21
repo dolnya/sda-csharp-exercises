09ddac09f5d7aa6cde3cef4a6590997f02951522
dolnya <90932597+dolnya@users.noreply.github.com>
2021-11-21 10:28:11 +0100

    1

---
+++ BasicAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sda_csharp_exercises
+{
+    internal class BasicAccount
+    {
+        public decimal Balance { get; private set; }
+        public decimal Intrest { get; private set; }
+
+        public BasicAccount(decimal balance = 0M)
+        {
+            Balance = balance;
+        }
+
+        public void IncreaseBalance(decimal value)
+        {
+            Balance += value;
+        }
+
+        public virtual decimal GetValue()
+        {
+            return Balance;
+        }
+
+	}
+}
--- Program.cs
+++ Program.cs
@@ -9,6 +9,17 @@ namespace sda_csharp_exercises
         //Rozwiąż zadanie E20 bez wykorzystywania dziedziczenia(utwórz jedną klasę BasicAccount).
         //Wskaż zalety i wady każdego z podejść.
         {
+            Bank bank = new Bank();
+            Account account = new Account(1500M);
+            account.IncreaseBalance(500);
+            SavingAccount savingAccount = new SavingAccount(4000, 100);
+            savingAccount.IncreaseInterest(50);
+
+            bank.AddAccount(account);
+            bank.AddAccount(savingAccount);
+
+            Console.WriteLine(bank.GetValue());
         }
+    }
     }
 }
